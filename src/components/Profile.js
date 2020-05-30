c875d7df11e659360fe16b6a88cb1dfd35279300
shinnaga <shinnaga.dev@gmail.com>
2020-05-31 02:12:54 +0900

    edit social-link add noopener

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,10 +6,10 @@ const Profile = () => {
     <div class='profile'>
       <div>my name is shinnaga.</div>
       <section className='social'>
-        <a href='https://www.wantedly.com/users/127147823'>wantedly</a>
-        <a href='https://lapras.com/public/MIRTA5Z'>Lapras</a>
-        <a href='https://github.com/sinjikoro'>github</a>
-        <a href='https://note.com/shinnaga'>note</a>
+        <a href='https://www.wantedly.com/users/127147823' target='_blank' rel='noopener noreferrer'>wantedly</a>
+        <a href='https://lapras.com/public/MIRTA5Z' target='_blank' rel='noopener noreferrer'>Lapras</a>
+        <a href='https://github.com/sinjikoro' target='_blank' rel='noopener noreferrer'>github</a>
+        <a href='https://note.com/shinnaga' target='_blank' rel='noopener noreferrer'>note</a>
       </section>
     </div>
   )
