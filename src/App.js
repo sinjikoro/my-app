d849c5672eb429c03113281e9264c5df8e6b5d8c
shinnaga <shinnaga.dev@gmail.com>
2020-06-01 02:13:24 +0900

    testing material-ui

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ import {
   BrowserRouter as Router
 } from 'react-router-dom';
 
+const menu = [
+  {title: 'Game', url:'/game'},
+  {title: 'Profile', url:'/profile'}
+];
+
 const App = () => {
   return (
     <React.Fragment>
       <Router>
-          <Header />
+          <Header title='shinnaga page' menu={menu} />
           <main>
             <Content />
             <Navigation />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Header.css';
-import {
-  Link
-} from 'react-router-dom';
-import {
-  Button
-} from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
 
-const Header = () => {
+export default function Header(props) {
+  const {title, menu} = props;
+
   return (
     <header className="head" role="navigation">
-      <h1 className="header_logo"><Link to="/">shinnaga site</Link></h1>
-      <ul className="header_nav">
-        <li><Link to="/game"><Button color='Primary'>Game</Button></Link></li>
-        <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
-      </ul>
+      <Toolbar>
+        <h1 className="header_logo"><Link to="/">{title}</Link></h1>
+      </Toolbar>
+      <Toolbar style={{justifyContent:'space-between', overflowX: 'auto'}}>
+        <ul className="header_nav">
+          <li><Link to="/game"><Button color='Primary'>Game</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>aaa</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+          <li><Link to="/profile"><Button color='Primary'>profile</Button></Link></li>
+        </ul>
+      </Toolbar>
     </header>
   )
 };
-
-export default Header;
